016212944412c1e91bc1dce7d3bf6ba9ae303b0e
hasan4993 <52563503+hasan4993@users.noreply.github.com>
2022-10-09 18:31:41 +0500

    Exercise 6 Completed - Ali Ul Hasan

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -1,7 +1,13 @@
 package com.serenitydojo.model;
 
 public class Feeder {
-    public String feeds(String animal, boolean isPremium) {
-        return "Food";
+    public String feeds(String animals, boolean isPremium) {
+        if (animals.equals("Cat")) {
+            return (isPremium) ? "Salmon" : "Tuna";
+        } else if (animals.equals("Dog")) {
+            return (isPremium) ? "Boiled Chicken" : "Dog Food";
+        } else {
+            return (isPremium) ? "Lettuce" : "Cabbage";
+        }
     }
 }
--- src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
@@ -40,6 +40,29 @@ public class WhenFeedingTheAnimals {
         String food = feeder.feeds("Cat", true);
 
         Assert.assertEquals("Salmon", food);
+        System.out.println(food);
+
+    }
+
+    @Test
+    public void shouldFeedPremiumDogsDeluxeDogFood() {
+        Feeder feeder = new Feeder();
+
+        String food = feeder.feeds("Dog", true);
+
+        Assert.assertEquals("Boiled Chicken", food);
+        System.out.println(food);
+
+    }
+
+    @Test
+    public void shouldFeedPremiumHamstersLettuce() {
+        Feeder feeder = new Feeder();
+
+        String food = feeder.feeds("Hamster", true);
+
+        Assert.assertEquals("Lettuce", food);
+        System.out.println(food);
 
     }
 }
